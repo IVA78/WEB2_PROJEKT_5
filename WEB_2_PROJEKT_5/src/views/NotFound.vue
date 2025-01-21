2f6f09ab581cd868ff440dda3fbdaefa6f3ab859
IvaSv <76878466+IVA78@users.noreply.github.com>
2025-01-21 18:27:08 +0100

    FINAL UPDATE

--- WEB_2_PROJEKT_5/src/views/NotFound.vue
+++ WEB_2_PROJEKT_5/src/views/NotFound.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="not-found">
-    <h1>Sorry, {{ $route.path }} is not found.</h1>
-    <p>Try going back to the <router-link to="/">homepage</router-link>.</p>
+    <h1>Sorry, page not found.</h1>
+    <p>Try going back to the <router-link to="/">homepage or use navigation bar.</router-link>.</p>
     <img src="../../public/images/404-error.png" alt="404 Error" />
   </div>
 </template>
